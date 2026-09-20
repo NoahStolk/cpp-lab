5901c5f8748423752df097af282f425c928a1fd6
Noah Stolk <noah.stolk@gmail.com>
2026-09-20 11:00:22 +0200

    Finish vec.h

--- src/apps/vec_demo/main.cpp
+++ src/apps/vec_demo/main.cpp
@@ -7,16 +7,20 @@ int main()
     constexpr const char* lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
 
-    const Vec3 pos = Vec3(1, 2, 3);
+    constexpr Vec3 pos = Vec3(1, 2, 3);
 
     std::cout << pos.len() << "\n";
     std::cout << pos.len_squared() << "\n";
 
-    const Vec3 dir = Vec3(1, 0, 0);
+    constexpr Vec3 dir = Vec3(1, 0, 0);
     std::cout << dir.z << "\n";
 
-    const Vec3 posB = pos + Vec3(4, 4, 4);
+    Vec3 posB = pos + Vec3(4, 4, 4);
     std::cout << posB << "\n";
+
+    posB += Vec3(1, 1, 1);
+    std::cout << posB << "\n";
+    std::cout << -posB << "\n";
 
     return 0;
 }
--- src/libs/lab_math/include/lab_math/vec.h
+++ src/libs/lab_math/include/lab_math/vec.h
@@ -9,40 +9,90 @@ struct Vec3
     float y;
     float z;
 
-    Vec3(const float x, const float y, const float z)
+    constexpr Vec3(const float x, const float y, const float z) noexcept
         : x(x), y(y), z(z)
     {
     }
 
-    Vec3 operator+(const Vec3 &vec) const
+    constexpr Vec3& operator+=(const Vec3& vec) noexcept
+    {
+        x += vec.x;
+        y += vec.y;
+        z += vec.z;
+        return *this;
+    }
+
+    [[nodiscard]] friend constexpr Vec3 operator+(Vec3 lhs, const Vec3& rhs) noexcept
+    {
+        return lhs += rhs;
+    }
+
+    constexpr Vec3& operator-=(const Vec3& vec) noexcept
+    {
+        x -= vec.x;
+        y -= vec.y;
+        z -= vec.z;
+        return *this;
+    }
+
+    [[nodiscard]] friend constexpr Vec3 operator-(Vec3 lhs, const Vec3& rhs) noexcept
+    {
+        return lhs -= rhs;
+    }
+
+    constexpr Vec3& operator*=(const float s) noexcept
+    {
+        x *= s;
+        y *= s;
+        z *= s;
+        return *this;
+    }
+
+    [[nodiscard]] friend constexpr Vec3 operator*(Vec3 vec, const float s) noexcept
+    {
+        return vec *= s;
+    }
+
+    [[nodiscard]] friend constexpr Vec3 operator*(const float s, Vec3 vec) noexcept
+    {
+        return vec *= s;
+    }
+
+    constexpr Vec3& operator/=(const float s) noexcept
+    {
+        x /= s;
+        y /= s;
+        z /= s;
+        return *this;
+    }
+
+    [[nodiscard]] friend constexpr Vec3 operator/(Vec3 vec, const float s) noexcept
+    {
+        return vec /= s;
+    }
+
+    [[nodiscard]] friend constexpr Vec3 operator-(const Vec3 vec) noexcept
     {
         return {
-            x + vec.x,
-            y + vec.y,
-            z + vec.z
+            -vec.x,
+            -vec.y,
+            -vec.z
         };
     }
 
-    Vec3 operator-(const Vec3 &vec) const
-    {
-        return {
-            x - vec.x,
-            y - vec.y,
-            z - vec.z
-        };
-    }
+    [[nodiscard]] constexpr bool operator==(const Vec3& vec) const = default;
 
     friend std::ostream& operator<<(std::ostream& os, const Vec3& vec)
     {
         return os << vec.x << ", " << vec.y << ", " << vec.z;
     }
 
-    [[nodiscard]] float len_squared() const
+    [[nodiscard]] constexpr float len_squared() const noexcept
     {
         return (x * x) + (y * y) + (z * z);
     }
 
-    [[nodiscard]] float len() const
+    [[nodiscard]] float len() const noexcept
     {
         return std::sqrt(len_squared());
     }
